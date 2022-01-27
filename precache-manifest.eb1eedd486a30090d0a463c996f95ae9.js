ec5e30b4af9981d443a8b547f067855121a6b6e6
Maria Rosolovska <feofanmaria19@gmail.com>
2022-01-27 13:51:59 +0200

    Updates

---
+++ precache-manifest.eb1eedd486a30090d0a463c996f95ae9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bac770b800102c1465f323306db6293",
+    "url": "/Gallery-React/index.html"
+  },
+  {
+    "revision": "d4bdb4617e98cc301883",
+    "url": "/Gallery-React/static/css/main.7431827b.chunk.css"
+  },
+  {
+    "revision": "5940bf03f3deef6ca053",
+    "url": "/Gallery-React/static/js/2.7497e026.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Gallery-React/static/js/2.7497e026.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4bdb4617e98cc301883",
+    "url": "/Gallery-React/static/js/main.c93b6783.chunk.js"
+  },
+  {
+    "revision": "d2fdbc9fd0b35a905289",
+    "url": "/Gallery-React/static/js/runtime-main.7d8db9c3.js"
+  }
+]);
